6e6bc45411a9d168dfcbddad4cd919629f1897af
slSeanWU <summer7sean@gmail.com>
2020-05-31 10:05:59 +0800

    add html login/signup templates

--- flask/routes.py
+++ flask/routes.py
@@ -1,6 +1,15 @@
 from flask import current_app as app
+from flask import render_template
 from models import User_Info
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/index', methods=['GET'])
+def show_index():
+    return render_template('index.html')
+
+@app.route('/signup', methods=['GET'])
+def show_signup():
+    return render_template('signup.html')

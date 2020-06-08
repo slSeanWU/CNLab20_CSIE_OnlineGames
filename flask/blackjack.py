531ee07d579c3f9df9d0826eac3823080312b88c
briany329 <brian88329@gmail.com>
2020-06-09 00:55:22 +0800

    some insignificant changes

--- flask/blackjack.py
+++ flask/blackjack.py
@@ -6,11 +6,9 @@ from flask_login import login_user, logout_user, login_required, current_user
 from flask_socketio import SocketIO, emit, disconnect
 import random
 
-
 cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 cards_player = []
 cards_dealer = []
-cards_dealer_show = []
 
 def blackjack_sum(cards_list):
     soft_sum = 0
@@ -45,36 +43,34 @@ def blackjack_play():
     user = current_user
     return render_template('blackjack_play.html', async_mode=socketio.async_mode, username=user.username, coins=user.coins)
 
-@socketio.on('connect_event', namespace='/test')
+@socketio.on('connect_event', namespace='/blackjack')
 def test_message(message):
     emit('server_response', {'data': message['data']})
 
-@socketio.on('client_event', namespace='/test')
+@socketio.on('client_event', namespace='/blackjack')
 def test_client_message(message):
     emit('server_response', {'data': message['data']+' Received.'})
 
-@socketio.on('disconnect_request', namespace='/test')
+@socketio.on('disconnect_request', namespace='/blackjack')
 def test_disconnect_request():    
     def can_disconnect():
         disconnect()
     emit('server_response', {'data': 'Disconnected!'}, callback=can_disconnect)
 
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect', namespace='/blackjack')
 def test_connect():
     emit('server_response', {'data': 'Server Connected!'})
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect', namespace='/blackjack')
 def test_disconnect():
     print('Client disconnected', request.sid)
 
-@socketio.on('start_game', namespace='/test')
-def test_start_game():
+@socketio.on('start_game', namespace='/blackjack')
+def start_game():
     global cards_player
-    global cards_dealer
-    global cards_dealer_show    
+    global cards_dealer 
     cards_player.clear()
     cards_dealer.clear()
-    cards_dealer_show.clear()
     cards_player = random.choices(cards, k=2)[:]
     cards_dealer = random.choices(cards, k=2)[:]
     cards_dealer_show = cards_dealer.copy()
@@ -82,16 +78,16 @@ def test_start_game():
     emit('start', {'player': cards_player, 'dealer': cards_dealer_show})
 
 
-@socketio.on('player_take_one', namespace='/test')
-def test_player_take_one():
+@socketio.on('player_take_one', namespace='/blackjack')
+def player_take_one():
     card = random.choice(cards)
     cards_player.append(card)
     emit('player_picked', {'cards': cards_player})
     if blackjack_sum(cards_player) > 21:
         emit('player_bust')
 
-@socketio.on('player_stop', namespace='/test')
-def test_player_stop():
+@socketio.on('player_stop', namespace='/blackjack')
+def player_stop():
     global cards_dealer
     while blackjack_sum(cards_dealer) < 17:
         cards_dealer.append(random.choice(cards))

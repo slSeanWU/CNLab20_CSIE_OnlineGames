fbdfd898c3fe8633a4cdb574bcc4380af19cab51
slSeanWU <summer7sean@gmail.com>
2020-06-19 09:51:36 +0800

    ask for db passwd from user input

--- flask/main.py
+++ flask/main.py
@@ -6,16 +6,21 @@ app = Flask(__name__)
 async_mode = None
 socketio = SocketIO(app, async_mode=async_mode)
 
-def create_app():
+def create_app(db_passwd):
     app.secret_key='secret key'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:final@35.236.142.37/Casino'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:' + db_passwd + '@35.236.142.37/Casino'
 
-    db.init_app(app)
-    with app.app_context():
-        import routes, slot, blackjack, poker
-        db.create_all()
-        db.session.commit()
+    try:
+        db.init_app(app)
+        with app.app_context():
+            import routes, slot, blackjack, poker
+            db.create_all()
+            db.session.commit()
+    except:
+        print ('Database connection failed, check your password!')
+        print ('Terminating ...')
+        exit()
 
     login_manager.init_app(app)
     login_manager.login_view = 'show_index'
@@ -25,5 +30,6 @@ def create_app():
     return app
 
 if __name__ == '__main__':
-    app = create_app()
+    db_passwd = input('enter the database password (maybe one more time): ').strip()
+    app = create_app(db_passwd)
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
